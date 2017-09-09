cdceb2f9ffa2e2bded144360660be45ab400aab1
Okura <vedha.ivan@gmail.com>
2017-09-08 21:51:14 -0500

    Mongo Pipeline para guardar lineas y estaciones

--- metro_scrap/pipelines.py
+++ metro_scrap/pipelines.py
@@ -1,11 +1,26 @@
 # -*- coding: utf-8 -*-
 
+from pymongo import MongoClient
+from metro_scrap.items.Linea import Linea
+from metro_scrap.items.Estacion import Estacion
+
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-class MetroScrapPipeline(object):
+class MongoDBPipeline(object):
+    def open_spider(self, spider):
+        self.client = MongoClient('localhost', 27017)
+        self.db = self.client['metro_df']
+
+    def close_spider(self, spider):
+        self.client.close()
+
     def process_item(self, item, spider):
+        if isinstance(item, Estacion):
+            self.db['Estaciones'].insert_one(dict(item))
+        if isinstance(item, Linea):
+            self.db['Linea'].insert_one(dict(item))
         return item
